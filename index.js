5321c7cdd6e78685e4cd457f5a67471bb4872095
Pint <83248746+janekv20@users.noreply.github.com>
2021-07-18 14:46:19 -0500

    updated Employee.js and index.js with code

---
+++ index.js
@@ -0,0 +1,8 @@
+//node modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//Extended Classes
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
