d37b67c88969f96ba2e07d8623c6eab16ed27d48
Pint <83248746+janekv20@users.noreply.github.com>
2021-07-18 17:11:09 -0500

    added function to add Employee data and role specific question to index.js

--- index.js
+++ index.js
@@ -6,3 +6,127 @@ const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+
+//team array
+const team = [];
+// function to add employee name, id, email, role and role specific questions
+function addEmployee() {
+    inquirer.prompt([{
+        //add employee name
+        type: 'input',
+        name: 'employee',
+        message: 'What is the name of the employee?',
+        //validate employee name input, else display message
+        validate: employeeInput => {
+            if (employeeInput) {
+                return true;
+            } else {
+                console.log('Please enter employee name!');
+                return false;
+            }
+        },
+        //add employee id
+        type: 'input',
+        name: 'id',
+        message: 'What is the id of the employee?',
+        //validate employee id input, else display message
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('Please enter employee id!');
+                return false;
+            }
+        },
+        //add employee email
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of the employee?',
+        //validate employee email input, else display message
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter employee email!');
+                return false;
+            }
+        },
+        //add role checkbox
+        type: 'checkbox',
+        name: 'role',
+        message: 'Select the role of the employee.',
+        choices: ['Engineer', 'Intern', 'Manager'],
+        //validate employee role checkbox, else display message
+        validate: roleInput => {
+            if (roleInput) {
+                return true;
+            } else {
+                console.log('Please select the employee role!');
+                return false;
+            }
+        }
+    }])
+        //function that checks selected role to display role specific questions
+        .then(function ({ name, id, email, role }) {
+            let roleData = "";
+            //Engineer role, ask for Github username
+            if (role === 'Engineer') {
+                roleData = 'Github username';
+                //Intern role, ask for School
+            } else if (role === 'Intern') {
+                roleData = 'School';
+                //If not engineer or intern, ask for office number.
+            } else {
+                roleData = 'Office number';
+            }
+            //prompt asking for role specific data
+            inquirer.prompt([{
+                message: `Enter employee's ${roleData}`,
+                name: 'roleData',
+                //validate if appropriate answer is inputted
+                validate: roleData => {
+                    if (roleData) {
+                        return true;
+                    } else {
+                        console.log('Please enter the ${roleData} for the employee!');
+                        return false;
+                    }
+                }
+            },
+            {//question asking if user would like to add more employees
+                type: 'checkbox',
+                message: 'Do you want to add more employees?',
+                name: moreEmployees,
+                choices: [
+                    'yes',
+                    'no'
+                ],
+                //validate that user selected a choice
+                validate: moreEmployeesInput => {
+                    if (moreEmployeesInput) {
+                        return true;
+                    } else {
+                        console.log('Please select an option!');
+                        return false;
+                    }
+                }
+            }])
+                //if user selected yes, then function will run that asks for employee data again and role specific data
+                .then(function ({ roleData, moreEmployees }) {
+                    let newEmployee;
+                    if (role === 'Engineer') {
+                        newEmployee = new Engineer(name, id, email, roleData);
+                    } else if (role === 'Intern') {
+                        newEmployee = new Intern(name, id, email, roleData);
+                    } else {
+                        newEmployee = new Manager(name, id, email, roleData);
+                    }
+                    team.push(newEmployee);
+                    if (moreEmployees) {
+                        return addEmployee(team);
+                    } else {
+                        return team;
+                    }
+                })
+        })
+};
