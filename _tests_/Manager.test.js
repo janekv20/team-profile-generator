cfd4b8b432595c300a306a0becc77d6e32be2a21
Pint <83248746+janekv20@users.noreply.github.com>
2021-07-18 23:52:41 -0500

    removed the quote around office number

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 
 test('creates a manager object', () => {
-    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', '12345');
+    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', 12345);
 
     expect(manager.name).toEqual(expect.any(String));
     expect(manager.id).toEqual(expect.any(Number));
@@ -9,31 +9,31 @@ test('creates a manager object', () => {
 });
 //get name
 test('gets manager name', () => {
-    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', '12345');
+    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', 12345);
 
     expect(manager.getName()).toEqual(expect.any(String));
 })
 //get id
 test('gets manager id', () => {
-    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', '12345');
+    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', 12345);
 
     expect(manager.getId()).toEqual(expect.any(Number));
 })
 //get email
 test('gets manager email', () => {
-    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', '12345');
+    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', 12345);
 
     expect(manager.getEmail()).toEqual(expect.any(String));
 })
 //get role
 test('gets manager role', () => {
-    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', '12345');
+    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', 12345);
 
     expect(manager.getRole()).toEqual(expect.any(String));
 })
 //get office number
 test('get manager office number', () => {
-    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', '12345');
+    const manager = new Manager('Jane Doe', 1, 'jane.h.vo@gmail.com', 12345);
 
     expect(manager.getOfficenumber()).toEqual(expect.any(Number));
 });
